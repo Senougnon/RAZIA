ec39a9037063681a588332a4568f57f2a123fdea
Senougnon <128873137+Senougnon@users.noreply.github.com>
2025-02-19 18:01:58 +0100

    Update script.js

--- DATA/Beta/script.js
+++ DATA/Beta/script.js
@@ -1,13 +1,13 @@
 // Initialisation de Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyBMioKaf2knW9BtrdUkrteEcIaKBJvG8JE",
+    apiKey: "edhBDEDBHUIBBH BDUHZSUBHUJ",
     authDomain: "razia-data.firebaseapp.com",
-    databaseURL: "https://razia-data-default-rtdb.firebaseio.com",
+    databaseURL: "https://hiruhiiu sibuiruhbziebu.com",
     projectId: "razia-data",
     storageBucket: "razia-data.appspot.com",
-    messagingSenderId: "797498750723",
-    appId: "1:797498750723:web:385d55e45ab4a0221b1b36",
-    measurementId: "G-F4MYMJVSRV"
+    messagingSenderId: "5648484565468468",
+    appId: "1:7975648452648565848858652",
+    measurementId: "G-FCBJKDHKBCHE"
 };
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
